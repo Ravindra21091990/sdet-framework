1dc53475a88f1c82ad0421f5028777e58eb28f66
Your Name <rvhyallij@gmail.com>
2026-03-26 19:56:32 +0530

    Docker implementation completed.

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,12 +1,14 @@
 package base;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utils.ConfigReader;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+
 
 public class BaseTest {
 
@@ -14,20 +16,25 @@ public class BaseTest {
     ConfigReader config = new ConfigReader();
 
     @BeforeMethod
-    public void setup(){
+    public void setup() {
 
-        WebDriverManager.chromedriver().setup();
+        try {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");   // 🔥 updated headless
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--window-size=1920,1080");
+            driver = new RemoteWebDriver(
+                    new URL("http://localhost:4444/wd/hub"),
+                    options
+            );
 
-        driver = new ChromeDriver(options); // ✅ ONLY ONE DRIVER
+            driver.manage().window().maximize();
+            driver.get(config.getProperty("url"));
 
-        driver.get(config.getProperty("url"));
-        System.out.println("Title: " + driver.getTitle());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterMethod
